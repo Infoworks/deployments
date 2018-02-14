f28879becf61955b4bc82e8cdc56fd29fb809624
saikrishna-v <35095062+saikrishna-v@users.noreply.github.com>
2018-02-14 15:14:20 +0530

    Update image_init.sh

--- gcp/deployment/configs/image_init.sh
+++ gcp/deployment/configs/image_init.sh
@@ -19,10 +19,27 @@ wget https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-latest-hadoop2.
 mv gcs-connector-latest-hadoop2.jar /usr/lib/hadoop/lib/
 wget https://storage.googleapis.com/hadoop-lib/bigquery/bigquery-connector-latest-hadoop2.jar
 mv bigquery-connector-latest-hadoop2.jar /usr/lib/hadoop/lib/
-apt-get -y install hive hive-jdbc hive-hcatalog tez --force-yes
+apt-get -y install hive hive-jdbc hive-hcatalog tez hbase --force-yes
 apt-get install python-setuptools libmysql-java
 ln -s /usr/share/java/mysql.jar /usr/lib/hive/lib/mysql.jar
 apt-get -y install spark-extras spark-core spark-python spark-datanucleus spark-r spark-yarn-shuffle --force-yes
 mkdir -p /hadoop/spark/{tmp,work}
 chmod -R 777 /hadoop/spark/
 chown -R spark:spark /hadoop/spark/
+wget https://github.com/Infoworks/deployments/raw/master/gcp/deployment/configs/hadoop_1.2.20.tar.gz
+rm -rf /etc/alternatives/hadoop-conf/*
+tar xzf hadoop_1.2.20.tar.gz -C /etc/alternatives/hadoop-conf/
+rm -rf hadoop_1.2.20.tar.gz
+wget https://github.com/Infoworks/deployments/raw/master/gcp/deployment/configs/spark_1.2.20.tar.gz
+rm -rf /etc/alternatives/spark-conf/*
+tar xzf spark_1.2.20.tar.gz -C /etc/alternatives/spark-conf/
+rm -rf spark_1.2.20.tar.gz
+cp -r /etc/alternatives/hive-conf/hive-site.xml /etc/alternatives/hive-hcatalog-conf/
+wget https://github.com/Infoworks/deployments/raw/master/gcp/deployment/configs/zoo_1.2.20.tar.gz
+rm -rf /etc/alternatives/zookeeper-conf/*
+tar xzf zoo_1.2.20.tar.gz -C /etc/alternatives/zookeeper-conf/
+rm -rf zoo_1.2.20.tar.gz
+wget https://github.com/Infoworks/deployments/raw/master/gcp/deployment/configs/hbase_1.2.20.tar.gz
+rm -rf /etc/alternatives/hbase-conf/*
+tar xzf hbase_1.2.20.tar.gz -C /etc/alternatives/hbase-conf/
+rm -rf hbase_1.2.20.tar.gz
