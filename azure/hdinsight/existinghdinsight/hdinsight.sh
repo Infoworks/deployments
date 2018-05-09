50f303504dc4f01e3df9d05d032f0310614f73e0
saikrishna-v <35095062+saikrishna-v@users.noreply.github.com>
2018-05-09 16:09:06 +0530

    Update hdinsight.sh

--- azure/hdinsight/existinghdinsight/hdinsight.sh
+++ azure/hdinsight/existinghdinsight/hdinsight.sh
@@ -6,10 +6,6 @@ export p3=$3
 export p4=$4
 
 export edgeNodeSetup="https://raw.githubusercontent.com/Infoworks/deployments/master/azure/hdinsight/existinghdinsight/edgenode-setup.sh"
-export sparkInstall="https://raw.githubusercontent.com/Infoworks/deployments/master/azure/hdinsight/existinghdinsight/spark-install.sh"
-
-#Print parameters
-echo "$p1 $p2 $p3 $p4" > /tmp/parameters.txt
 
 _timestamp(){
 	date +%H:%M:%S
@@ -41,23 +37,14 @@ _is_edgenode()
 _init(){
 
 	_download_file ${edgeNodeSetup} /tmp/edgeNodeSetup.sh
-	_download_file ${sparkInstall} /tmp/sparkInstall.sh
 
 	sed -i 's/\r//g' /tmp/edgeNodeSetup.sh
-	sed -i 's/\r//g' /tmp/sparkInstall.sh
 
 	if [ $(_is_edgenode) == 1 ]; then
 		eval /bin/bash /tmp/edgeNodeSetup.sh $p1 $p2 $p3
-		sleep 5
-		if [ $p4 != False ]; then
-		eval /bin/bash /tmp/sparkInstall.sh $p1 $p2 $p3
-		fi
-	else
-		eval /bin/bash /tmp/sparkInstall.sh $p1 $p2 $p3	
 	fi
 	
 	rm -rf /tmp/edgeNodeSetup.sh
-	rm -rf /tmp/sparkInstall.sh
 }
 
 _init
