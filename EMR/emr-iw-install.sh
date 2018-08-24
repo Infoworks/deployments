8df368fafd9ff055d6d9aa3b2fb3d4ea2f38cd00
saikrishna-v <35095062+saikrishna-v@users.noreply.github.com>
2018-08-24 17:24:23 +0530

    Update emr-iw-install.sh

--- EMR/emr-iw-install.sh
+++ EMR/emr-iw-install.sh
@@ -108,8 +108,20 @@ _delete_tar(){
     fi
 }
 
+##Replacing MasterNode Hostname in respective nodes.
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/hbase/conf/hbase-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/tez/conf/tez-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/hadoop/conf/yarn-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/hadoop/conf/core-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/hadoop/conf/hdfs-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/hadoop/conf/mapred-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/spark/conf/spark-defaults.conf
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/spark/conf/spark-env.sh
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/spark/conf/hive-site.xml
+sed -i -e "s/{{MASTER_HOSTNAME}}/${Masternode}/g" /etc/hive/conf/hive-site.xml
 sudo -u hdfs hdfs dfs -mkdir /user/${username}
 sudo -u hdfs hdfs dfs -chown -R ${username}:${username} /user/${username}
+
 
 ##Running Infoworks
 eval _create_user && _download_app && _deploy_app && [ -f $configured_status_file ] && _delete_tar && echo "Application deployed successfully"  || echo "Deployment failed"
