54b18370d54b05f3df7dfc69666b16eb68926fcf
saikrishna-v <35095062+saikrishna-v@users.noreply.github.com>
2019-06-13 00:04:00 +0530

    Added Cluster Type dependency

--- azure/hdinsightv2/backup-scripts/HDIUtilities.sh
+++ azure/hdinsightv2/backup-scripts/HDIUtilities.sh
@@ -146,7 +146,7 @@ function ClusterType
 {
   CLUSTERTYPE=$(echo -e "import hdinsight_common.ClusterManifestParser as ClusterManifestParser\nprint ClusterManifestParser.parse_local_manifest().settings[Constants.CLUSTER_TYPE]" | python)
   if [ $? -ne 0 ]; then
-    echo "[ERROR] Cannot determine cluster name. Exiting!"
+    echo "[ERROR] Cannot determine cluster Type. Exiting!"
     exit 134
   fi
 }
