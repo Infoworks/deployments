4a0ffee1cc6c05f6869c1a86a2d5cae5ebee7539
saikrishna-v <35095062+saikrishna-v@users.noreply.github.com>
2019-08-19 12:16:15 +0530

    QA Package

--- azure/hdinsight/existinghdinsight/edgenode-setup.sh
+++ azure/hdinsight/existinghdinsight/edgenode-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-version=2.7.2
-export app_path=https://infoworks-setup.s3.amazonaws.com/2.7/infoworks-$version-azure.tar.gz
+version=2.8.0_beta1
+export app_path=https://infoworks-setup.s3.amazonaws.com/2.8/infoworks-$version-azure.tar.gz
 export app_name=infoworks
 export iw_home=/opt/${app_name}
 export configured_status_file=$iw_home/conf/configured
