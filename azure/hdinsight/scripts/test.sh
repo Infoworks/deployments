de0bef8092378b1840275397b00b0feb7d4785c7
saikrishna-v <saikrishna@infoworks.io>
2020-05-09 19:51:44 +0530

    Test

---
+++ azure/hdinsight/scripts/test.sh
@@ -0,0 +1,7 @@
+#!/bin/#!/usr/bin/env bash
+echo "Checking Args"
+echo "Deployment: $1" >> ~/temp
+echo "DB_INSTANCE: $2" >> ~/temp
+echo "DB_URL: $3" >> ~/temp
+echo "DB_TOKEN: $4" >> ~/temp
+echo "DNS_NAME: $5" >> ~/temp
