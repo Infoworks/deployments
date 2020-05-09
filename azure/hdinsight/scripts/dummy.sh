c6277a932e1dedd70a7eaeacd1ff335d13a6e189
saikrishna-v <saikrishna@infoworks.io>
2020-05-09 19:00:07 +0530

    Added Temp DNS Settings entry

--- azure/hdinsight/scripts/dummy.sh
+++ azure/hdinsight/scripts/dummy.sh
@@ -20,6 +20,9 @@ export DNS_NAME=$5
 readonly HOSTNAME=`hostname -f`
 export DF_USER=$(grep 'IW_USER' /opt/iw-installer/configure.sh | cut -f2 -d'=')
 
+if [ -f /run/cloud-init/instance-data.json ]; then
+  export DNS_SETTINGS=$(grep -i 'location' /run/cloud-init/instance-data.json | awk '{print $2}' | tr -d '",')
+fi
 if getent passwd $DF_USER; then
   echo "User already exists"
 else
@@ -34,7 +37,7 @@ sed -i -e "s|^export\ IW_DB_CLUSTER_TIMEOUT.*$|export\ IW_DB_CLUSTER_TIMEOUT=60|
 sed -i -e "s|^export\ DB_URL.*$|export\ DB_URL=$DB_URL|" /opt/iw-installer/configure.sh
 sed -i -e "s|^export\ DB_TOKEN.*$|export\ DB_TOKEN=$DB_TOKEN|" /opt/iw-installer/configure.sh
 sed -i -e "s|^export\ IW_EDGENODE_IP.*$|export\ IW_EDGENODE_IP=$HOSTNAME|" /opt/iw-installer/configure.sh
-sed -i -e "s|^proxy_server_host.*$|proxy_server_host=$DNS_NAME|" /opt/infoworks/conf/conf.properties.default
-systemctl restart collectd
+sed -i -e "s|^proxy_server_host.*$|proxy_server_host=$DNS_NAME.$DNS_SETTINGS.cloudapp.azure.com|" /opt/infoworks/conf/conf.properties.default || true
 
 su -c 'pushd /opt/iw-installer && source configure.sh && ./configure_install.sh && ./install.sh -v 3.2.0-adb-ubuntu && popd || echo "Deployment Failed"' -s /bin/bash $DF_USER
+systemctl restart collectd
